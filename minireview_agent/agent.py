1b1f3e194fa44b9782942f95f36d03c90fa6df28
Timmatt <lee.timmatt@gmail.com>
2025-10-13 15:57:53 +0800

    refactor: simplify model definition in agent

--- minireview_agent/agent.py
+++ minireview_agent/agent.py
@@ -1,12 +1,10 @@
 from google.adk.agents import LlmAgent
-from google.adk.models import Gemini
 from google.adk.tools.mcp_tool.mcp_toolset import McpToolset
 from google.adk.tools.mcp_tool.mcp_session_manager import StdioConnectionParams
-from google.genai import types
 from mcp import StdioServerParameters
 
 root_agent =  LlmAgent(
-    model=Gemini(model='gemini-2.5-pro', retry_options=types.HttpRetryOptions(initial_delay=1, attempts=2),),
+    model='gemini-2.5-pro',
     name='MiniReview',
     instruction='You are a mobile game expert. You can help users find games, get details about them, and much more. You are using the MiniReview API to get the information.',
     tools=[
