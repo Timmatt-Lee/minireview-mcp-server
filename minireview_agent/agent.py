39731df77c927782fd1d8f6dce121ad190caf118
Timmatt <lee.timmatt@gmail.com>
2025-10-13 14:22:35 +0800

    Update agent to automatically save session to memory

--- minireview_agent/agent.py
+++ minireview_agent/agent.py
@@ -1,10 +1,16 @@
 from google.adk.agents import LlmAgent
+from google.adk.models import Gemini
 from google.adk.tools.mcp_tool.mcp_toolset import McpToolset
 from google.adk.tools.mcp_tool.mcp_session_manager import StdioConnectionParams
 from mcp import StdioServerParameters
+from google.adk.tools.load_memory_tool import LoadMemoryTool
 
-root_agent = LlmAgent(
-    model='gemini-2.5-flash',
+async def auto_save_session_to_memory_callback(callback_context):
+    await callback_context._invocation_context.memory_service.add_session_to_memory(
+        callback_context._invocation_context.session)
+
+root_agent =  LlmAgent(
+    model=Gemini(),
     name='MiniReview',
     instruction='You are a mobile game expert. You can help users find games, get details about them, and much more. You are using the MiniReview API to get the information.',
     tools=[
@@ -19,6 +25,8 @@ root_agent = LlmAgent(
                 ),
                 timeout=30,
             ),
-        )
+        ),
+        LoadMemoryTool()
     ],
+    after_agent_callback=auto_save_session_to_memory_callback,
 )
